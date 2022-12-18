4fee23901c6f1d3278a1a4383754addc496e4baa
Mark Taranenko <taranenkom7@gmail.com>
2022-12-18 18:40:18 +0200

    UPLOADING EVERYTHING

---
+++ loadUI.py
@@ -0,0 +1,211 @@
+from PyQt6.QtWidgets import QStackedWidget ,QTextEdit, QRadioButton, QCheckBox, QMainWindow,QApplication, QLineEdit,QWidget, QLabel, QPushButton
+from PyQt6 import uic
+import sys
+from Attempt2 import *
+from PyQt6.QtGui import QPixmap,QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        
+        uic.loadUi("UI.ui",self)
+        
+        self.setWindowTitle("Router Search")
+        self.setWindowIcon(QIcon("logo.png"))
+        self.search_text = self.findChild(QLineEdit, 'lineEdit')
+        self.searchButton=self.findChild(QPushButton, 'pushButton')
+        self.nextButton=self.findChild(QPushButton, 'nextButton')
+        self.model_title=self.findChild(QLabel, 'modelTitle')
+        self.previousButton=self.findChild(QPushButton, 'previousButton')
+        self.image = self.findChild(QLabel, 'image')
+        self.changeWindowButton = self.findChild(QPushButton, 'changeWindowButton')
+        
+        #For tracking
+        self.routers=[]
+        self.i=0
+        
+        #self.image=self.findChild(QGraphicsView, 'imageView')
+        
+        #Actions
+        self.searchButton.clicked.connect(self.search)
+        self.search_text.returnPressed.connect(self.search)
+        # self.nextButton.clicked.connect(self.next)
+        # self.previousButton.clicked.connect(self.previous)
+        # self.changeWindowButton.clicked.connect(self.changeWindow)
+        self.central_widget = self.findChild(QStackedWidget,"stackedWidget")
+        self.homePage()
+        #Define the UI elements
+        
+    def searchReview(self):
+        key_words = self.key_words.text()
+        is_long = self.is_long.isChecked()
+        stars = None
+        if self.allstars.isChecked():
+            self.reviews = self.item.getReviewFromCriterias(key_words, None, is_long)
+        else:
+            for i in range(5):
+                if self.stars[i].isChecked():
+                    stars = i+1
+                    break
+            self.reviews = self.item.getReviewFromCriterias(key_words, stars, is_long)
+        self.review_index = 0
+        if self.reviews:
+            self.nextReviewButton.clicked.connect(self.nextReview)
+            self.previousReviewButton.clicked.connect(self.previousReview)
+            self.showReview(self.reviews, self.review_index)
+           # self.star_ico_review.setHidden(False)
+        else:
+            self.noReview()
+    def showReview(self,reviews,index):
+        self.username.setText(reviews[index].username)
+        self.star_user.setText(str(reviews[index].stars))
+        self.text_review.setPlainText(reviews[index].comment)
+        #self.star_ico_review.setHidden(False)
+    def nextReview(self):
+        if len(self.reviews)==0:
+            return
+        try:
+            self.review_index+=1
+            if(self.review_index>=len(self.reviews)):
+                self.review_index=0
+            self.showReview(self.reviews,self.review_index)
+        except:
+            self.noReview()
+    def previousReview(self):
+        if len(self.reviews)==0:
+            return
+        try:
+            self.review_index-=1
+            if(self.review_index<0):
+                self.review_index=len(self.reviews)-1
+            self.showReview(self.reviews,self.review_index)
+        except:
+            self.noReview()
+    def returnToMain(self):
+        self.homePage()
+        
+    def homePage(self):
+        #uic.loadUi('AlphaForRouterSearch.ui', self)
+        self.central_widget.setCurrentWidget(self.findChild(QWidget,"home_page"))
+    def noReview(self):
+        self.text_review.setPlainText("No review found")
+        self.username.setText("")
+        self.star_user.setText("")
+        #self.image.mouseReleaseEvent = self.changeWindow()
+        #self.show()
+    def itemPage(self):
+        # super(ItemWindow,self).__init__()
+        # self.setWindowTitle("Router Search")
+        # self.setWindowIcon(QIcon("logo.png"))
+        
+        self.central_widget.setCurrentWidget(self.findChild(QWidget,"item_page"))
+        #self.main = MainWindow
+        self.item = self.routers[self.i]
+        self.returnButton = self.findChild(QPushButton,"returnButton")
+        self.image2 = self.findChild(QLabel,"image_2")
+        self.search_button = self.findChild(QPushButton,"searchButton")
+        self.model_title2 = self.findChild(QLabel,"modelTitle_2")
+        self.av_stars = self.findChild(QLabel,"avStars")
+        self.key_words = self.findChild(QLineEdit,"keyWords")
+        self.is_long = self.findChild(QCheckBox, 'isLong')
+        self.text_review = self.findChild(QTextEdit,"textReview")
+        self.username = self.findChild(QLabel,"usernameReview")
+        self.star_user = self.findChild(QLabel,"starReview")
+        self.star_ico_review = self.findChild(QLabel,"starIcoReview")
+        self.nextReviewButton = self.findChild(QPushButton,"nextReviewButton")
+        self.previousReviewButton = self.findChild(QPushButton,"previousReviewButton")
+        self.allstars = self.findChild(QRadioButton,"starAll")
+        self.stars = []
+        for j in range(5):
+            self.stars.append(self.findChild(QRadioButton, f'star{j+1}'))
+        #Actions
+        self.key_words.returnPressed.connect(self.searchReview)
+        self.search_button.clicked.connect(self.searchReview)
+        self.returnButton.clicked.connect(self.returnToMain)
+        self.image2.setPixmap(QPixmap(self.item.image_path))
+        self.model_title2.setText(self.item.title)
+        self.av_stars.setText(str(self.item.av_stars))
+        self.noReview()
+        self.text_review.setPlainText("")
+        #self.star_ico_review.setHidden(True)
+        #self.show()
+    
+    def changeWindow(self):
+        if len(self.routers)==0:
+            return
+        self.routers[self.i].downloadReviews()
+        self.itemPage()
+        
+        #self.hide()
+    def previous(self):
+        if len(self.routers)==0:
+            return
+        try:
+            self.i-=1
+            if(self.i<0):
+                self.i=len(self.routers)-1
+            self.showRouter(self.i)
+        except:
+            self.noRoutersFound()
+            pass
+        
+    def noRoutersFound(self):
+        self.model_title.setText("No routers found")
+        self.pixmap = QPixmap("crying_cat.jpg")
+        self.image.setPixmap(self.pixmap)
+        
+    def showRouter(self,i):
+        self.model_title.setText(self.routers[i].title)
+        self.pixmap = QPixmap(self.routers[i].image_path)
+         #add pixmap to label
+        self.image.setPixmap(self.pixmap)
+        
+    def next(self):
+        if len(self.routers)==0:
+            return
+        try:
+            self.i+=1
+            if(self.i>=len(self.routers)):
+                self.i=0
+            self.showRouter(self.i)
+        except:
+            self.noRoutersFound()
+    def search(self):
+        self.routers = []
+        try:
+            self.i = 0
+        except:
+            pass
+        URL = "https://hotline.ua/ua/computer/besprovodnoe-oborudovanie/?q="+formatingSearch(self.search_text.text())
+        addFormatedItems(getHTMLtext(URL),self.routers)
+        try:
+            self.model_title.setText(self.routers[0].title)
+            self.showRouter(0)
+            self.nextButton.clicked.connect(self.next)
+            self.previousButton.clicked.connect(self.previous)
+            self.changeWindowButton.clicked.connect(self.changeWindow)
+        except:
+            self.noRoutersFound()
+        
+
+if __name__ == "__main__":
+    createFolder("Images")
+    
+    app = QApplication(sys.argv)
+    
+    window = MainWindow()
+    window.show()
+    # routers = []
+    # home_page = MainWindow(routers)
+    # item_page = ItemWindow(routers,0,)
+    
+    # window_stack = QStackedWidget()
+    # window_stack.addWidget(home_page)
+    # window_stack.addWidget(item_page)
+    # home_page.changeWindowButton.clicked.connect(lambda: window_stack.setCurrentIndex(1))
+    
+    # window_stack.show()
+    app.exec()
+    
+    deleteImages()
+
---
+++ main.py
@@ -0,0 +1,176 @@
+from cgitb import text
+from xml.etree.ElementTree import iterparse
+import urllib3 #internet requests
+from bs4 import BeautifulSoup #HTML text formattig and managing
+import re #for text formatig
+import unidecode
+import os
+from math import floor
+import urllib.request #for image downloading
+# html_text = requests.get('https://hotline.ua/ua/computer/besprovodnoe-oborudovanie/?q=Xiaomi').text
+# print(html_text)
+
+def deleteCharsFromText(text, chars):
+    for char in chars:
+        text=text.replace(char,"")
+    return text
+class Review:#Need getReviews method for working
+    def __init__(self,comment,stars, user):
+        self.comment = comment
+        self.stars = unidecode.unidecode(textFormat(stars))
+        self.username = user
+    def print(self):
+        print(f"User: {self.username}")
+        print(f"Review text: {self.comment}")
+        print(f"Stars: {self.stars}")
+    def keyWordsSearch(self, key_words):
+        key_words = deleteCharsFromText(key_words,",.!()?{[}]\|").lower().split()
+        comment_text = self.comment.lower()
+        for word in key_words:
+            if word not in comment_text:
+                return False
+        return True
+    def countWords(self):#roughly
+        return self.comment.count(" ")+1
+    def isInStars(self, stars):
+        return stars==None or (stars == floor(float(self.stars)))
+    def isInCritarias(self, key_words, stars, isLong):
+        min_words = 0
+        if isLong:
+            min_words = 25
+        return self.keyWordsSearch(key_words) and self.isInStars(stars) and self.countWords()>=min_words
+class Item:
+    #items_list = []#for all Item members
+    def isImageExist(self):
+        return os.path.exists("Images/"+self.title+".jpg")
+    def __init__(self,title, link, price, short_specefication):
+        self.title  = title
+        self.link = link
+        self.price = price
+        self.specefication = short_specefication
+        self.description = str() #TODO
+        self.reviews = [] #will add later TODO
+        self.image_path = None
+        #self.av_stars = 0
+        if(self.isImageExist()):
+            self.image_path = f"Images/{self.title}.jpg"
+        
+        # html = getHTMLtext(link)
+        # self.average_rating = html.find("div",class_="informer-review__rating-value").text
+    
+    def downloadReviews(self):
+        html = getHTMLtext(self.link+"?tab=reviews&filter=reviews")
+        try:
+            self.av_stars = float(html.find("div",class_="informer-review__rating-value").text)
+        except:
+            self.av_stars = 0
+        reviews_html = html.find_all("div", class_ ="review__col-content")
+        for review in reviews_html:
+            try:
+                review_text = review.find("div", class_ = "review__comment").text
+                review_stars = review.find("div", class_="review__rating-value").text
+                reviewer_name = review.find("a", class_="review__user-name").text
+            except:
+                continue
+            self.reviews.append(Review(review_text, review_stars,reviewer_name))
+        #print(self.av_stars)
+    def print(self):
+        print(f"Title: {self.title}")
+        print(f"Price: {self.price}")
+        print(f"Link: {self.link}")
+        #print(f"Specefication: {self.specefication}")
+        try:
+            print(f"Average rating: {self.av_stars}")
+        except:
+            pass
+    def getInfo(self):
+        info = []
+        info.append(self.title)
+        info.append(self.price)
+        info.append(self.link)
+        return info
+    def getReviewFromCriterias(self, key_words, stars, isLong):#return first appearence
+        good_reviews = []
+        for review in self.reviews:
+            if review.isInCritarias(key_words, stars, isLong):
+                good_reviews.append(review)
+            else:
+                1+2
+        return good_reviews
+        
+
+        
+def getHTMLtext(URL):#search_text):#text version of HTML page
+    #url = 'https://hotline.ua/ua/computer/besprovodnoe-oborudovanie/?q='+formatingSearch(search_text)
+    http = urllib3.PoolManager()
+    resp = http.request('GET', URL)
+    html_text = resp.data.decode('utf-8')
+    soup = BeautifulSoup(html_text, 'lxml')
+    return soup
+
+def textFormat(txt):# Just for text formating(delete the spaces between words)
+    return re.sub(' +', ' ',txt.replace('\n', '').replace('\t','').lstrip().rstrip())
+
+# def getFileExtension(url):
+#     return url.split('.')[-1]
+
+def addFormatedItems(html_txt, items_class):#from Hotline page
+    items = html_txt.find_all('div', class_='list-item list-item--row')#not formated
+    #items_formated = list()
+    for item in items:
+        #item_formated= list()
+        item_link= "https://hotline.ua" + item.find_all('a')[1].get('href')#magic number
+        item_title = textFormat(item.find('a', class_='list-item__title text-md').text)
+        item_price = unidecode.unidecode(textFormat(item.find('span', class_='price__value').text))
+        item_specification = textFormat(item.find('div', class_='list-item__specifications-text').text)
+        
+        try:
+            item_img_url = "https://hotline.ua"+item.find('img').get('src')
+            urllib.request.urlretrieve(item_img_url, f"Images/{item_title}.jpg")
+            #print(f"Title: {item_title}\nURL: {item_img_url}\n")
+        except Exception as e:
+            continue
+            #print(f"Title: {item_title}\nURL: {item_img_url}\nError: {e}\n")
+            pass
+            
+
+        items_class.append(Item(item_title,item_link,item_price,item_specification))
+
+def printItem(item):
+    print(f"Title: {item[0]}")
+    print(f"Link: {item[1]}")
+    print(f"Price: {item[2]}")
+def printItems(items):
+    for item in items:
+        printItem(item)
+        print('')
+def formatingSearch(search):#For ability to search with spaces between words
+    return search.replace(" ","%20") 
+
+def createFolder(name):
+    try:
+        os.mkdir(name)
+    except:
+        pass
+    
+
+def deleteImages():
+    for file in os.listdir("Images"):
+        os.remove(f"Images/{file}")
+
+# createFolder("Images")
+
+# search = "Xiaomi Mi WiFi Router 4A Gigabit Edition"
+# # search = str(input("Enter what are you looking for: "))
+# items_class = []
+# addFormatedItems(getHTMLtext("https://hotline.ua/ua/computer/besprovodnoe-oborudovanie/?q="+formatingSearch(search)),items_class)
+
+
+# items_class[0].downloadReviews()
+# key_words = "працює"
+# review = items_class[0].getReviewFromCriterias(key_words, None, False)
+# if review:
+#     review.print()
+
+#deleteImages()
+#input("\n\nEnter to exit....")
